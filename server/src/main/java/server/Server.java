a94a893f7f5ce987f6e231c3121e42224f68dcbd
derekharker <derekharker4@gmail.com>
2025-10-20 13:45:35 -0600

    Server.java with Spark

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,37 @@
 package server;
 
-import io.javalin.*;
+import handler.*;
+import spark.*;
+import dataaccess.memory.MemoryGame;
+import dataaccess.memory.MemoryAuth;
+import dataaccess.memory.MemoryUser;
 
 public class Server {
 
-    private final Javalin javalin;
-
-    public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
-
-    }
-
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        Spark.port(desiredPort);
+        Spark.staticFiles.location("web");
+
+        MemoryUser userDAO = new MemoryUser();
+        MemoryAuth authDAO = new MemoryAuth();
+        MemoryGame gameDAO = new MemoryGame();
+
+        //Register endpoints
+        Spark.delete("/db", new clearHandler(userDAO, authDAO, gameDAO));
+        Spark.post("/user", new RegisterHandler(userDAO, authDAO));
+        Spark.delete("/session", new LogoutHandler(userDAO, authDAO));
+        Spark.post("/session", new LoginHandler(userDAO, authDAO));
+        Spark.post("/game", new CreateGameHandler(authDAO, gameDAO));
+        Spark.put("/game", new JoinGameHandler(authDAO, gameDAO));
+        Spark.get("game", new ListGamesHandler(authDAO, gameDAO));
+
+        Spark.awaitInitialization();
+        return Spark.port();
     }
 
     public void stop() {
-        javalin.stop();
+        Spark.stop();
+        Spark.awaitStop();
+
     }
 }

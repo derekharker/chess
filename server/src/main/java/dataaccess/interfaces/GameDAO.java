fe988422ac481349e854eec606f928893588353e
derekharker <derekharker4@gmail.com>
2025-10-20 12:54:12 -0600

    almost there with GameDAO.java

---
+++ server/src/main/java/dataaccess/interfaces/GameDAO.java
@@ -0,0 +1,16 @@
+package dataaccess.interfaces;
+
+import model.GameData;
+import response.JoinGameResponse;
+import chess.ChessGame;
+
+import java.util.Collection;
+
+public interface GameDAO {
+    void clearApplication(AuthDAO authData, UserDAO userData);
+    int createGame(String gameName);
+    void clearGames();
+    JoinGameResponse updateUserInGame(int gameID, String username, ChessGame.TeamColor teamColor);
+    Collection<GameData> listGames();
+
+}

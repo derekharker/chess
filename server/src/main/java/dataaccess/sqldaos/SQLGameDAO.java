e3a134fb040dfbbe9f8b465591053d79641d0d43
derekharker <derekharker4@gmail.com>
2025-10-30 15:17:54 -0600

    Unused method in SQLGameDAO.java

--- server/src/main/java/dataaccess/sqldaos/SQLGameDAO.java
+++ server/src/main/java/dataaccess/sqldaos/SQLGameDAO.java
@@ -179,16 +179,15 @@ public class SQLGameDAO implements GameDAO {
         }
     }
 
-
-    private boolean updateGame(int gameID, ChessGame game) {
-        String st = "UPDATE game SET game_info = ? WHERE game_id = ?";
-        String gameString = (String) Translation.fromObjectToJson(game);
-        try {
-            executeUpdate(st, gameID, gameString);
-            return true;
-        } catch (DataAccessException e) {
-            System.out.println("Error creating a game: " + e.getMessage());
-            return false;
-        }
-    }
+//    private boolean updateGame(int gameID, ChessGame game) {
+//        String st = "UPDATE game SET game_info = ? WHERE game_id = ?";
+//        String gameString = (String) Translation.fromObjectToJson(game);
+//        try {
+//            executeUpdate(st, gameID, gameString);
+//            return true;
+//        } catch (DataAccessException e) {
+//            System.out.println("Error creating a game: " + e.getMessage());
+//            return false;
+//        }
+//    }
 }

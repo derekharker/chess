6a5f865fd00370e4a79b354546c0bf577cbcae04
derekharker <derekharker4@gmail.com>
2025-11-08 16:55:32 -0700

    more in BoardCreation.java

--- client/src/main/java/ui/BoardCreation.java
+++ client/src/main/java/ui/BoardCreation.java
@@ -2,6 +2,8 @@ package ui;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -61,6 +63,71 @@ public class BoardCreation {
                 drawRow(out, j, board, teamColor);
             }
         }
+    }
+
+    private void drawRow(PrintStream out, int row, ChessBoard board, ChessGame.TeamColor teamColor) {
+        assert row > 0 && row < 9;
+
+        printRowNum(out, row);
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            for (int i = 1; i < 9; i++) {
+                printSquareInfo(out, row, i, board);
+            }
+        } else {
+            for (int j = 8; j > 0; j--) {
+                printSquareInfo(out, row, j, board);
+            }
+        }
+
+        printRowNum(out, row);
+        out.print(RESET_TEXT_COLOR);
+        out.print(RESET_BG_COLOR);
+        out.println();
+    }
+
+    private void printSquareInfo(PrintStream out, int i, int j, ChessBoard board) {
+        assert i > 0 && i < 9;
+
+        //Choosing the BG
+        if (isLight(i, j)) {
+            setLight(out);
+        } else {
+            setDark(out);
+        }
+
+        //Is piece or not
+        if (isPiece(i, j, board)) {
+            printPieceInfo(out, i, j, board);
+        } else {
+            out.print(EMPTY.repeat(3));
+        }
+    }
+
+    private void printPieceInfo(PrintStream out, int i, int j, ChessBoard board) {
+        ChessPosition pos = new ChessPosition(i, j);
+        String pieceType = pieceLetter(board.getPiece(pos).getPieceType());
+
+        ChessGame.TeamColor teamColor = board.getPiece(pos).getTeamColor();
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            setWhiteText(out);
+        } else {
+            setBlackText(out);
+        }
+
+        out.print(EMPTY);
+        out.print(pieceType);
+        out.print(EMPTY);
+    }
+
+    private String pieceLetter(ChessPiece.PieceType type) {
+        return switch (type) {
+            case PAWN -> "P";
+            case KING -> "K";
+            case QUEEN -> "Q";
+            case BISHOP -> "B";
+            case ROOK -> "R";
+            case KNIGHT -> "N";
+        };
     }
 
 

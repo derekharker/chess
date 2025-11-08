5f18f237e33d574910de919667d4118365c4e99d
derekharker <derekharker4@gmail.com>
2025-11-08 16:28:39 -0700

    createHeaders function

--- client/src/main/java/ui/BoardCreation.java
+++ client/src/main/java/ui/BoardCreation.java
@@ -14,5 +14,24 @@ public class BoardCreation {
         createHeaderList(out, teamColor);
     }
 
+    private void createHeaderList(PrintStream out, ChessGame.TeamColor teamColor) {
+        assert teamColor == ChessGame.TeamColor.WHITE || teamColor == ChessGame.TeamColor.BLACK;
 
+        setBackground(out);
+        drawEmptySquare(out);
+
+        String[] headers = {"A", "B", "C", "D", "E", "F", "G", "H"};
+
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            for (int i = 0; i < 8; ++i) {
+                drawHeader(out, headers[i]);
+            }
+        } else {
+            for (int j = 7; j >= 0; --j) {
+                drawHeader(out, headers[j]);
+            }
+        }
+
+
+    }
 }

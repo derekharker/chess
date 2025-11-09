42eb536fa283ec0085c9c7201fbca74e53317135
derekharker <derekharker4@gmail.com>
2025-11-09 14:09:08 -0700

    Added in Translator for future tasks

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,14 @@
 import chess.*;
+import ui.BoardCreation;
+import ui.ClientMenu;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        new ClientMenu(port).run();
     }
 }
---
+++ client/src/main/java/translatorClient/ClientTranslation.java
@@ -0,0 +1,15 @@
+package translatorClient;
+
+import com.google.gson.Gson;
+
+public class ClientTranslation {
+    private static Gson GSON = new Gson();
+
+    public static Object fromObjectToJson(Object result){
+        return GSON.toJson(result);
+    }
+
+    public static <T> T fromJsontoObjectNotRequest(String string, Class<T> classOfT){
+        return GSON.fromJson(string, classOfT);
+    }
+}

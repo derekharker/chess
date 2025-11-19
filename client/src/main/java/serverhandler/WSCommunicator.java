4d8468337877a7639b9803c50afd9af98545d9f4
derekharker <derekharker4@gmail.com>
2025-11-19 15:47:13 -0700

    WSCommunicator function

--- client/src/main/java/serverhandler/WSCommunicator.java
+++ client/src/main/java/serverhandler/WSCommunicator.java
@@ -1,4 +1,42 @@
 package serverhandler;
 
-public class WSCommunicator {
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WSCommunicator extends Endpoint {
+    String url;
+    Session session;
+    ServerMessageObserver observer;
+
+    public WSCommunicator(String url, ServerMessageObserver observer) {
+        try {
+            this.url = url.replace("http", "ws");
+            URI socketURI = new URI(this.url + "/ws");
+            this.observer = observer;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = TranslatorForClient.
+                            fromJsontoObjectNotRequest(message, ServerMessage.class);
+                    observer.notify(notification);
+                }
+            });
+        } catch (URISyntaxException | DeploymentException | IOException ex) {
+            System.out.println("Error connecting to websocket. In WS Communicator: " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndPointConfig endPointConfig) {
+
+    }
+
+
 }

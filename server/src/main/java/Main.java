d503a3b11c82f434f4c03f61ef44893ca07ef789
derekharker <derekharker4@gmail.com>
2025-11-25 10:40:38 -0700

    ClientTranslation more changes

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,11 @@
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import server.Server;
 import chess.*;
 
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
         Server server = new Server();
         server.run(8080);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);

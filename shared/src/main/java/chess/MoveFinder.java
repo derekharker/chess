d2564c3973f8eb0833125f17eb06f2869660284b
derekharker <derekharker4@gmail.com>
2026-04-28 11:53:34 -0600

    doublemove functionality

--- shared/src/main/java/chess/MoveFinder.java
+++ shared/src/main/java/chess/MoveFinder.java
@@ -15,8 +15,8 @@ public class MoveFinder {
     private List<ChessPiece.PieceType> promotionP = Arrays.asList(ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT,
             ChessPiece.PieceType.ROOK, ChessPiece.PieceType.BISHOP);
 
-    public MoveFinder(chess.ChessBoard board, chess.ChessPosition pos, Collection<chess.ChessMove> moves,
-                      chess.ChessGame.TeamColor teamColor) {
+    public MoveFinder(ChessBoard board, ChessPosition pos, Collection<ChessMove> moves,
+                      ChessGame.TeamColor teamColor) {
         this.board = board;
         this.pos = pos;
         this.moves = moves;
@@ -36,6 +36,56 @@ public class MoveFinder {
     }
 
     private void promotePawn(ChessPosition newPosition) {
+        for (ChessPiece.PieceType piece : promotionP) {
+            ChessMove move = new ChessMove(pos, newPosition, piece);
+            moves.add(move);
+        }
+    }
 
+    private void moveDirection(int rowMove, int colMove) {
+        int newRowPos = row + rowMove;
+        int newColPos = col + colMove;
+
+        if (newRowPos > 8 || newColPos > 8 ||  newRowPos < 1 || newColPos < 1) {
+            return;
+        }
+
+        boolean doubleMove = false;
+        boolean promotion = false;
+
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            if (row == 2 && colMove == 0) {
+                doubleMove = true;
+            }
+            if (row == 7) {
+                promotion = true;
+            }
+        }
+
+        if (teamColor == ChessGame.TeamColor.BLACK) {
+            if (row == 7 && colMove == 0) {
+                doubleMove = true;
+            }
+            if (row == 2) {
+                promotion = true;
+            }
+        }
+
+        ChessPosition newPos = new ChessPosition(newRowPos, newColPos);
+        ChessPiece newPiece = board.getPiece(newPos);
+
+        if (colMove == 0) {
+            if (newPiece == null) {
+                if (promotion) {
+                    promotePawn(newPos);
+                } else {
+                    ChessMove newMove = new ChessMove(pos, newPos, null);
+                    moves.add(newMove);
+                    if (doubleMove) {
+                        ChessPosition doublePos
+                    }
+                }
+            }
+        }
     }
 }

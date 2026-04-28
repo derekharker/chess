d95a5451cedd00c1beab69ab662a46b8ab0ebce1
derekharker <derekharker4@gmail.com>
2026-04-28 10:42:52 -0600

    Starting movefinders

---
+++ shared/src/main/java/chess/MoveFinder.java
@@ -0,0 +1,25 @@
+package chess;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class MoveFinder {
+
+    private final ChessBoard board;
+    private final ChessPosition pos;
+    private final Collection<ChessMove> moves;
+    private final ChessGame.TeamColor teamColor;
+    private int row;
+    private int col;
+    private List<ChessPiece.PieceType> promotionP = Arrays.asList(ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.ROOK, ChessPiece.PieceType.BISHOP);
+
+    public MoveFinder(chess.ChessBoard board, chess.ChessPosition pos, Collection<chess.ChessMove> moves,
+                      chess.ChessGame.TeamColor teamColor) {
+        this.board = board;
+        this.pos = pos;
+        this.moves = moves;
+        this.teamColor = teamColor;
+    }
+}

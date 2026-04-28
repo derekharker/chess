3712a2cab10975eea9d004da82c84febf12bcfcf
derekharker <derekharker4@gmail.com>
2026-04-28 13:50:20 -0600

    more work on movefinder

--- shared/src/main/java/chess/MoveFinder.java
+++ shared/src/main/java/chess/MoveFinder.java
@@ -19,6 +19,56 @@ public class MoveFinder {
         this.pieceType = pieceType;
     }
 
+    public void pieceLogic() {
+        switch(pieceType) {
+            case KING:
+                moveDirSingle(1,0);
+                moveDirSingle(1,1);
+                moveDirSingle(1,-1);
+                moveDirSingle(-1,1);
+                moveDirSingle(-1,0);
+                moveDirSingle(-1,-1);
+                moveDirSingle(0,1);
+                moveDirSingle(0,-1);
+                break;
+            case QUEEN:
+                moveDirMultiple(1,1);
+                moveDirMultiple(1,-1);
+                moveDirMultiple(1,0);
+                moveDirMultiple(-1,1);
+                moveDirMultiple(-1,0);
+                moveDirMultiple(-1,-1);
+                moveDirMultiple(0,1);
+                moveDirMultiple(0,-1);
+                break;
+            case KNIGHT:
+                moveDirSingle(2,1);
+                moveDirSingle(-2,1);
+                moveDirSingle(2,-1);
+                moveDirSingle(-2,-1);
+                moveDirSingle(1,2);
+                moveDirSingle(1,-2);
+                moveDirSingle(-1,2);
+                moveDirSingle(-1,-2);
+                break;
+            case ROOK:
+                moveDirMultiple(1,0);
+                moveDirMultiple(-1,0);
+                moveDirMultiple(0,1);
+                moveDirMultiple(0,-1);
+                break;
+            case BISHOP:
+                moveDirMultiple(1,1);
+                moveDirMultiple(1,-1);
+                moveDirMultiple(-1,1);
+                moveDirMultiple(-1,-1);
+                break;
+        }
+    }
+
+    private void moveDirSingle(int rowDirection, int colDirection) {
+
+    }
 
 
 }

f397032b9567d0bcaa1c8808936d44bfafd07a29
derekharker <derekharker4@gmail.com>
2026-04-28 10:53:52 -0600

    do I need a promote pawn?

--- shared/src/main/java/chess/MoveFinder.java
+++ shared/src/main/java/chess/MoveFinder.java
@@ -22,4 +22,20 @@ public class MoveFinder {
         this.moves = moves;
         this.teamColor = teamColor;
     }
+
+    public void calculateMoves() {
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            moveDirection(1,0);
+            moveDirection(1,1);
+            moveDirection(1,-1);
+        } else {
+            moveDirection(-1,0);
+            moveDirection(-1,1);
+            moveDirection(-1,-1);
+        }
+    }
+
+    private void promotePawn(ChessPosition newPosition) {
+
+    }
 }

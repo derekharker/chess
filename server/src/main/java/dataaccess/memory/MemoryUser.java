97d3ea19c3ee1ef641bfa0390a6c6673ce19b9c8
derekharker <derekharker4@gmail.com>
2026-05-15 14:45:41 -0600

    Memory method for user

--- server/src/main/java/dataaccess/memory/MemoryUser.java
+++ server/src/main/java/dataaccess/memory/MemoryUser.java
@@ -1,4 +1,47 @@
 package dataaccess.memory;
 
-public class MemoryUser {
+import dataaccess.interfaces.UserDAO;
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUser implements UserDAO {
+
+    private final Map<String, UserData> users = new HashMap<>();
+
+    public MemoryUser() {
+    }
+
+    @Override
+    public boolean isVerifiedUser(String username, String password) {
+
+        UserData user = users.get(username);
+
+        if (user == null) {
+            return false;
+        }
+
+        return user.password().equals(password);
+    }
+
+    public void clearUsers() {
+        users.clear();
+    }
+
+    public UserData createUser(UserData newUser) {
+
+        users.put(newUser.username(), newUser);
+
+        return newUser;
+    }
+
+    public UserData getUser(String username) {
+
+        if (!users.containsKey(username)) {
+            return null;
+        }
+
+        return users.get(username);
+    }
 }

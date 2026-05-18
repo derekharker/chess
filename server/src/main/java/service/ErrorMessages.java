db53d9ec93053eaaf72e089d037b7912958af04b
derekharker <derekharker4@gmail.com>
2026-05-18 15:28:03 -0600

    Add SQL error to messages

--- server/src/main/java/service/ErrorMessages.java
+++ server/src/main/java/service/ErrorMessages.java
@@ -8,5 +8,6 @@ public class ErrorMessages {
     public static final String UNAUTHORIZED = "Error: unauthorized";
     public static final String ALREADYTAKEN = "Error: already taken";
     public static final String BADREQUEST = "Error: bad request";
+    public static final String SQLERROR = "Error: SQL error";
 
 }

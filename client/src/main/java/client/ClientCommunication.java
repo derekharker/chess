38777e2c7752d76e67e5cf0a1917bae9fc529cf1
derekharker <derekharker4@gmail.com>
2026-06-02 09:18:48 -0600

    client communicate with server start

--- client/src/main/java/client/ClientCommunication.java
+++ client/src/main/java/client/ClientCommunication.java
@@ -1,4 +1,56 @@
 package client;
 
+import com.google.gson.Gson;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+
 public class ClientCommunication {
+    private final String serverURL;
+    private final Gson gson = new Gson();
+
+    public ClientCommunication(int port) {
+        this.serverURL = "http://localhost:" + port;
+    }
+
+    public <T> T get(String path, String authToken, Class<T> responseClass) throws ClientException {
+        return makeRequest("GET", path, null, authToken, responseClass);
+    }
+
+    public <T> T post(String path, Object requestBody, String authToken, Class<T> responseClass) throws ClientException {
+        return makeRequest("POST", path, requestBody, authToken, responseClass);
+    }
+
+    public <T> T put(String path, Object requestBody, String authToken, Class<T> responseClass) throws ClientException {
+        return makeRequest("PUT", path, requestBody, authToken, responseClass);
+    }
+
+    public <T> T delete(String path, String authToken, Class<T> responseClass) throws ClientException {
+        return makeRequest("DELETE", path, null, authToken, responseClass);
+    }
+
+    private <T> T makeRequest(
+            String method,
+            String path,
+            Object requestBody,
+            String authToken,
+            Class<T> responseClass
+    ) throws ClientException {
+        try {
+            // creating url and exception handling stuff
+            var url = URI.create(serverURL + path).toURL();
+
+
+            var connection = (HttpURLConnection)url.openConnection();
+
+            connection.setRequestProperty(method);
+
+
+
+        } catch (ClientException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ClientException("Unable to talk to the server on MySQL ; sorry");
+        }
+    }
 }

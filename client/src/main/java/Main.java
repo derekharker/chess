8223808482e8c23059ffb77a610b77143eaf7184
derekharker <derekharker4@gmail.com>
2026-06-01 18:49:01 -0600

    ClientMenu.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,6 @@
-package client;
-
 import chess.*;
 
-public class ClientMain {
+public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);

b6fc15afc30cac4990afde88f57708dbd41fe25b
derekharker <derekharker4@gmail.com>
2026-06-02 09:05:10 -0600

    client main

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,9 +1,27 @@
 package client;
 
+import menu.ClientMenu;
+
+import java.util.Scanner;
+
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        var scanner = new Scanner(System.in);
+        var menu = new ClientMenu();
 
+        System.out.println("♕ Welcome to Chess!");
+        System.out.println("Type help to get started.");
+
+        while (true) {
+            System.out.print(">>> ");
+            String line = scanner.nextLine();
+
+            String result = menu.readPostLoginResponse(line);
+            System.out.println(result);
+
+            if (line.trim().equalsIgnoreCase("quit")) {
+                break;
+            }
+        }
     }
 }

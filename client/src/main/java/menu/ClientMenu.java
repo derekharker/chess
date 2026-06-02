a24194e6d7af28aa1d137c4213931904e5674c04
derekharker <derekharker4@gmail.com>
2026-06-01 19:11:29 -0600

    return String methods

--- client/src/main/java/menu/ClientMenu.java
+++ client/src/main/java/menu/ClientMenu.java
@@ -22,7 +22,7 @@ public class ClientMenu {
                 case "join" -> join(tokens[1], tokens[2]);
                 case "observe" -> observe(tokens[1]);
                 case "logout" -> logout();
-                case "quit" -> quit();
+                case "quit" -> quitChess();
                 default -> postLoginHelp();
             };
         } catch (Exception e) {
@@ -30,7 +30,37 @@ public class ClientMenu {
         }
     }
 
-    private void postLoginHelp() {
+    private String postLoginHelp() {
+        return "If you want to create a game, enter: create <game name>\n" +
+                "If you want to list all games currently in the database, enter: list\n" +
+                "If you want to join a present game, enter: join <game number> <WHITE|BLACK>\n" +
+                "If you want to observe a game happening, enter: observe <game number>\n" +
+                "If you want to logout when done, enter: logout\n" +
+                "If you want to quit chess, enter: quit\n" +
+                "Enter help again if you want to see this beautiful menu again :)\n";
+    }
 
+    private String create(String name) {
+        return "created game";
+    }
+
+    private String list() {
+        return "list";
+    }
+
+    private String join(String name, String color) {
+
+    }
+
+    private String observe(String name) {
+
+    }
+
+    private String logout() {
+        return "Logged out successfully";
+    }
+
+    private String quitChess() {
+        return "Bye!";
     }
 }

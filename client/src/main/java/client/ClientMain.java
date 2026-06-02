fa50f3334769f37d487bc5ceebe9625ec90307f4
derekharker <derekharker4@gmail.com>
2026-06-02 09:04:12 -0600

    pom.xml need clientmain here

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,6 +1,6 @@
-import chess.*;
+package client;
 
-public class Main {
+public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
